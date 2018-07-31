61706423726cfa1e54a3861678940a9cdd0bc0f3
Alex Mouton <thomas.mouton@gmail.com>
2018-07-31 14:15:30 -0700

    renamed det invDet.

--- src/Data/Matrix3.purs
+++ src/Data/Matrix3.purs
@@ -46,19 +46,19 @@ normalFromMat4 (Mat [a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23,
       b10 = a21 * a33 - a23 * a31
       b11 = a22 * a33 - a23 * a32
         -- Calculate the determinant
-      det_ = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06
+      det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06
 
-  in if (det_ == 0.0)
+  in if (det == 0.0)
         then Nothing
-        else let det = 1.0 / det_
+        else let invDet = 1.0 / det
               in Just $ Mat [
-                      (a11 * b11 - a12 * b10 + a13 * b09) * det,
-                      (a12 * b08 - a10 * b11 - a13 * b07) * det,
-                      (a10 * b10 - a11 * b08 + a13 * b06) * det,
+                      (a11 * b11 - a12 * b10 + a13 * b09) * invDet,
+                      (a12 * b08 - a10 * b11 - a13 * b07) * invDet,
+                      (a10 * b10 - a11 * b08 + a13 * b06) * invDet,
 
-                      (a02 * b10 - a01 * b11 - a03 * b09) * det,
-                      (a00 * b11 - a02 * b08 + a03 * b07) * det,
-                      (a01 * b08 - a00 * b10 - a03 * b06) * det,
+                      (a02 * b10 - a01 * b11 - a03 * b09) * invDet,
+                      (a00 * b11 - a02 * b08 + a03 * b07) * invDet,
+                      (a01 * b08 - a00 * b10 - a03 * b06) * invDet,
 
                       (a31 * b05 - a32 * b04 + a33 * b03) * det,
                       (a32 * b02 - a30 * b05 - a33 * b01) * det,
